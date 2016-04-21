65139eaa33dd28b846826d630730450d2f7ea8dd
alejandroav <alejandroalarcon93@gmail.com>
2016-04-21 17:47:37 +0200

    bliblbi

---
+++ Src/cabecera.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <string.h>
+#include <sstream>
+#include "json.hpp"
+using namespace std;
