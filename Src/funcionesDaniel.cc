2fcdc71897ab8318805e214ce08c3fbd54c32a41
Daniel Finestrat <danielfinestratmartinez@hotmail.com>
2016-04-28 18:28:20 +0200

    Añadido menú y manageArguments

--- Src/funcionesDaniel.cc
+++ Src/funcionesDaniel.cc
@@ -1,36 +1,39 @@
 #include "cabecera.h"
 
+int menu(){
+	int toRet = 0;
+	cout<<"Menu de peticiones:\n\r1.- Listado de edificios\n\r2.- Edificios con estancias sin ocupantes\n\r3.- Edificios + Estancias + Numero de Ocupantes\n\r";
+	cin>>toRet;
+	if (toRet <= 0 || toRet >= 4) toRet = menu();
+	return(toRet);
+}
+
 int manageArguments(int argc, char *argv[]){
 
 	int toRet = 0;
+	string idEdificio, idActividad = idEdificio = "";
 
-	/*string err = "Error. Se debe indicar el puerto del servidor y la ruta al archivo de configuracion\n\rSintaxis: http_server [puerto] [-c fichero_configuracion]\n\r";
+	string err = "ERROR. Sintaxis: "; err += + argv[0]; err += " [-e id_edificio [-a id_actividad]]\n\r";
+	err += "Ejemplo: "; err += + argv[0]; err += " -e 0001 -a 20\n\r";
 
-	if(argc == 2) puerto = atoi(argv[1]);
-	
-	if(argc == 3){ if(strcmp(argv[1],"-c") == 0){ rutaConf = argv[2]; } else toRet = 1; }
+	if(argc == 2 || argc == 4 || argc >= 6) { toRet = -1; }
 
-	if(argc == 4){
-		if(strcmp(argv[1],"-c") == 0){ rutaConf = argv[2]; puerto = atoi(argv[3]); }
-		else if(strcmp(argv[2],"-c") == 0){ rutaConf = argv[3]; puerto = atoi(argv[1]); }
-		else toRet = 1;
+	else if(argc == 3) { 
+		if(strcmp(argv[1],"-e") == 0){
+			idEdificio = argv[2];
+			toRet = 2;
+		} else toRet = -1;
 	}
 
-	if(toRet == 1 || argc > 4){ toRet = 1; cout << err; }*/
+	else if(argc == 5) {
+		if(strcmp(argv[1],"-e") == 0 && strcmp(argv[3],"-a") == 0){
+			idEdificio = argv[2];
+			idActividad = argv[4];
+			toRet = 5;
+		} else toRet = -1;
+	}
 
+	if (toRet == 0) toRet = menu();
+	if (toRet == -1) cout<<err; return(toRet);
 	
-	/*if (argc !=  4){
-		fprintf(stderr, "Error. Debe indicar la direccion del servidor (IP y Puerto) y el mensaje a enviar\r\n");
-		fprintf(stderr, "Sintaxis: %s <ip> <puerto> <mensaje>\n\r", argv[0]);
-		fprintf(stderr, "Ejemplo : %s 192.168.6.7 8574 \"Esto es un mensaje\"\n\r", argv[0]);
-		return 1;
-	}	
-	servidor_ip = argv[1];
-	servidor_puerto = argv[2];
-	mensaje = argv[3];
-
-	printf("\n\rEnviar mensaje \"%s\" a %s:%s...\n\r\n\r", mensaje, servidor_ip, servidor_puerto);*/
-
-	return toRet;
-
 }
