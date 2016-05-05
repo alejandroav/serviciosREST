16880851c2e35d758d003d14d5384de7a80967a7
angelnm <angel.nm@hotmail.es>
2016-05-05 19:06:58 +0200

    Update funcionesDaniel.cc

--- Src/funcionesDaniel.cc
+++ Src/funcionesDaniel.cc
@@ -2,16 +2,16 @@
 
 int menu(){
 	int toRet = 0;
-	cout<<"Menu de peticiones:\n\r1.- Listado de edificios\n\r2.- Numero de estancias y ocupantes por edificio\n\r3.- Edificios con estancias sin ocupantes\n\r";
+	cout<<"Menu de peticiones:\n\r1.- Listado de edificios\n\r2.- Numero de estancias y ocupantes por edificio\n\r3.- Edificios con estancias sin ocupantes\n\r4.-Estancias por edificio\n\r";
 	cin>>toRet;
-	if (toRet <= 0 || toRet >= 4) toRet = menu();
+	if (toRet <= 0 || toRet >= 6) toRet = menu();
 	return(toRet);
 }
 
 int manageArguments(int argc, char *argv[]){
 
 	int toRet = 0;
-	string idEdificio, idActividad = idEdificio = "";
+    string idEdificio, idActividad;
 
 	string err = "ERROR. Sintaxis: "; err += + argv[0]; err += " [-e id_edificio [-a id_actividad]]\n\r";
 	err += "Ejemplo: "; err += + argv[0]; err += " -e 0001 -a 20\n\r";
